8903c1037c69117db277631ba803f1e27b2377dd
Pahul Panesar <pahulpn@gmail.com>
2019-04-19 18:33:04 -0700

    beginner project setup

---
+++ jsx/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Hi!
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
