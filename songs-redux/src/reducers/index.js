dd96ae67a8d221a29f1908f4bccbcd711afc11f2
Pahul Panesar <pahulpn@gmail.com>
2019-05-08 16:28:35 -0700

    finished song creator using redux

---
+++ songs-redux/src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Can't Tell Me Nothing", duration: "3:39" },
+    { title: "Old Town Road", duration: "2:55" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
